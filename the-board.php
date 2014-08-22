87117c3b70987e4b6486a0794f0cf32ad529622e
gabrielstuff <gabriel@soixantecircuits.fr>
2014-08-22 11:04:16 +0200

    fixed the taxo issue

--- the-board.php
+++ the-board.php
@@ -68,9 +68,10 @@ add_action( 'plugins_loaded', array( 'The_Board', 'get_instance' ) );
  *
  * The code below is intended to to give the lightest footprint possible.
  */
-if ( is_admin() && ( ! defined( 'DOING_AJAX' ) || ! DOING_AJAX ) ) {
+if ( is_admin()  ) {
 
 	require_once( plugin_dir_path( __FILE__ ) . 'admin/the-board-admin.php' );
+  //add_action( 'init', array( 'The_Board_Admin', 'tb_member_groups_taxonomies_init' ) );
 	add_action( 'plugins_loaded', array( 'The_Board_Admin', 'get_instance' ) );
 
 }
