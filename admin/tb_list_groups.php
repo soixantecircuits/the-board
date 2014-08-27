952d640b134797be1ca2ed654dea91a9df11e2a8
hugo <debian@debian>
2014-08-27 18:53:12 +0200

    group shortcode input is now readonly

--- admin/tb_list_groups.php
+++ admin/tb_list_groups.php
@@ -17,7 +17,7 @@ function groups_columns( $return, $column, $group_id ) {
     $group = get_term( $group_id, 'groups' );
   switch ( $column ) {
     case 'shortcode' :
-      $return = "<input type=\"text\" value=\"[theboard-show-group group=$group->term_id]\">";
+      $return = "<input type=\"text\" value=\"[theboard-show-group group=$group->term_id]\" readonly>";
       break;
   }
   return $return;
