9ea52597c023a5997ca5484d85c290348a201f18
Shiyue Wang <shiyue.wang@me.com>
2014-09-03 13:54:35 +0200

    update plugin version

--- public/the-board.php
+++ public/the-board.php
@@ -26,7 +26,7 @@ class The_Board {
    *
    * @var     string
    */
-  const VERSION = '1.0.8';
+  const VERSION = '1.0.019';
 
   /**
    * @theboard - Rename "the-board" to the name of your plugin
